298dae11d5c259b69e25167cb5c71b97d3d7c827
Casmir Onyeka <casmir293@gmail.com>
2024-08-21 00:29:32 +0100

    feat: installed tailwind and vuetify.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section>Hello, World!</section>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <button
+    class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+  >
+    ...
+  </button>
+
+  <v-btn variant="outlined"> Button </v-btn>
 </template>
